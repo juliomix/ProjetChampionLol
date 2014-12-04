7cd565cb6bc418384df76cb6659e7e6bd55ad43f
martinhoury <martinhoury@gmail.com>
2014-12-04 09:40:21 +0100

    Suppression du projet netbeans

--- ApplicationLOL/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Jules Aimé\Documents\NetBeansProjects\Champion_Lol\bin\AndroidManifest.xml : \
-C:\Users\Jules Aimé\Documents\NetBeansProjects\Champion_Lol\AndroidManifest.xml \
--- ApplicationLOL/bin/Champion_Lol-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-C:\Users\Jules Aimé\Documents\NetBeansProjects\Champion_Lol\bin\Champion_Lol-debug-unaligned.apk : \
-C:\Users\Jules Aimé\Documents\NetBeansProjects\Champion_Lol\bin\Champion_Lol.ap_ \
-C:\Users\Jules Aimé\Documents\NetBeansProjects\Champion_Lol\bin\classes.dex \
